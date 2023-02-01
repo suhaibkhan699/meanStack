0e0579f9b9c8903c40fcba763154fc4454fcd4b2
suhaibkhan699 <khansuhaib699@gmail.com>
2023-02-01 21:55:24 +0000

    backend optimized

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,114 +1,19 @@
 const express = require('express')
-const multer = require('multer')
 const router = express.Router()
-const Post = require('../models/post')
 const checkAuth = require('../middleware/check-auth')
+const PostController = require('../controllers/posts')
+const extractFile = require('../middleware/file')
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype]
-    let error = new Error("invalid mime type")
-    if (isValid) {
-      error = null
-    }
-    cb(error, "backend/images")
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-')
-    const ext = MIME_TYPE_MAP[file.mimetype]
-    cb(null, name + '-' + Date.now() + '.' + ext)
-  }
-})
-
-router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host')
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + '/images/' + req.file.filename,
-    creator: req.userData.userId
-  })
-  post.save().then((post) => {
-    res.status(201).json({
-      post,
-      message: 'Post added successfully'
-    })
-  })
-})
+router.post('', checkAuth, extractFile, PostController.createPost)
 
 router.put('/:id', checkAuth,
-  multer({ storage: storage }).single('image'),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath
-    if (req.file) {
-      const url = req.protocol + '://' + req.get('host')
-      imagePath = url + '/images/' + req.file.filename
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId
-    })
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).
-      then((result) => {
-        if (result.modifiedCount) {
-          res.status(200).json({
-            message: 'Post updated successfully!'
-          })
-        } else {
-          res.status(401).json({
-            message: 'Not authorized!!!'
-          })
-        }
-      })
-  })
+  extractFile, PostController.updatePost
+)
 
-router.get('', (req, res, next) => {
-  Post.find().then((docs) => {
-    res.status(200).json({
-      message: 'posts fethced successfully',
-      posts: docs
-    })
-  })
-})
+router.get('', PostController.getPosts)
 
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).
-    then((post) => {
-      if (post) {
-        res.status(200).json({ post })
-      }
-      else {
-        res.status(404).json({
-          message: 'Post not found'
-        })
-      }
-    })
-})
+router.get('/:id', PostController.getPost)
 
-router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({
-    _id: req.params.id,
-    creator: req.userData.userId
-  }).then((result) => {
-    if (result.deletedCount) {
-      res.status(200).json({
-        message: 'Post deleted successfully!'
-      })
-    } else {
-      res.status(401).json({
-        message: 'Not authorized!!!'
-      })
-    }
-  })
-})
+router.delete('/:id', checkAuth, PostController.deletePost)
 
 module.exports = router
